843fa363205c96651ee1f1597a6d42861bc67295
zhanglang <350976906@qq.com>
2022-08-01 23:00:57 +0800

    跨域配置

---
+++ src/main/java/com/pda/common/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.pda.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Classname CorsConfig
+ * @Description TODO
+ * @Date 2022-08-01 22:52
+ * @Created by AlanZhang
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * 开启跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
