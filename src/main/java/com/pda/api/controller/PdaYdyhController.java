403981fbe47dfd7d09535ece078e621f8c85a8ca
zhanglang <350976906@qq.com>
2022-09-14 19:07:45 +0800

    皮试医嘱

--- src/main/java/com/pda/api/controller/PdaYdyhController.java
+++ src/main/java/com/pda/api/controller/PdaYdyhController.java
@@ -2,6 +2,8 @@ package com.pda.api.controller;
 
 import com.pda.api.service.PdaService;
 import com.pda.common.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @Created by AlanZhang
  */
 @RestController
+@Api(tags = "床位管理")
 public class PdaYdyhController {
 
     @Autowired
     private PdaService pdaService;
 
+    @ApiOperation(value = "床位管理")
     @GetMapping("/bed/{wardCode}")
     public Result myPatients(@PathVariable("wardCode") String wardCode){
         return Result.success(pdaService.beds(wardCode,1));
--- src/main/java/com/pda/common/config/WebConfig.java
+++ src/main/java/com/pda/common/config/WebConfig.java
@@ -22,6 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
-        registry.addInterceptor(userInterceptor).addPathPatterns("/**").excludePathPatterns("/pda/login").excludePathPatterns("/doc.html");
+        registry.addInterceptor(userInterceptor).addPathPatterns("/**").excludePathPatterns("/pda/login").excludePathPatterns("/pda/doc.html");
     }
 }
