b34b5d408967bfebd979714d271f8406aa25b749
zhanglang <350976906@qq.com>
2022-09-18 16:04:13 +0800

    皮试医嘱

--- src/main/java/com/pda/utils/PdaTimeUtil.java
+++ src/main/java/com/pda/utils/PdaTimeUtil.java
@@ -3,6 +3,10 @@ package com.pda.utils;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -54,31 +58,16 @@ public class PdaTimeUtil {
     }
 
     public static String getAgeStr(Date birth) {
-        Calendar cal = Calendar.getInstance();
-        int thisYear = cal.get(Calendar.YEAR);
-        int thisMonth = cal.get(Calendar.MONTH);
-        int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-
-        cal.setTime(birth);
-        int birthYear = cal.get(Calendar.YEAR);
-        int birthMonth = cal.get(Calendar.MONTH);
-        int birthdayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-
-        int age = thisYear - birthYear;
-
-        // 未足月
-        if (thisMonth <= birthMonth) {
-            // 当月
-            if (thisMonth == birthMonth) {
-                // 未足日
-                if (dayOfMonth < birthdayOfMonth) {
-                    age--;
-                }
-            } else {
-                age--;
-            }
-        }
-        return String.format("%s岁%s月",age,(thisMonth - birthMonth));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String birthStr = sdf.format(birth);
+        String now = sdf.format(new Date());
+        Temporal temporal1 = LocalDate.parse(birthStr);
+        Temporal temporal2 = LocalDate.parse(now);
+        // 方法返回为相差月份
+        long l = ChronoUnit.MONTHS.between(temporal1, temporal2);
+        long years = l/12;
+        long monthes = l%12;
+        return String.format("%s岁%s月",years,monthes);
     }
 
     public static Integer getDurationDays(Date startDate,Date endDate) {
@@ -96,12 +85,10 @@ public class PdaTimeUtil {
 
 
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String str = "2022-09-13 16:00:37";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String str = "1951-12-01 16:00:37";
         Date date = sdf.parse(str);
         System.out.println(getAgeStr(date));
-
-        System.out.println(getDurationDays(date,new Date()));
     }
 
 }
