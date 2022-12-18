b75d9c210d49ee2097ee9e0b7f5187fa33a3ed8c
zhanglang <350976906@qq.com>
2022-12-18 12:26:22 +0800

    提交

--- src/main/java/com/pda/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pda/exception/GlobalExceptionHandler.java
@@ -5,10 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @Classname GlobalExceptionHandler
@@ -29,5 +36,33 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         String code = e.getCode();
         return StringUtils.isNotBlank(code) ? Result.failed(code, e.getMessage()) : Result.failed(e.getMessage());
+    }
+
+    //捕获校验失败的异常
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    //设置响应状态码，当然这里其实我并没有用上，我自己在R_对象封装了
+    @ResponseStatus(HttpStatus.OK)
+    public Result handleParamCheckExcepion(HttpServletRequest req, MethodArgumentNotValidException ex) {
+        //那下面这里就是读取具体的校验失败的数据了，可能不是一个，所以其实有一个遍历的过程。
+        BindingResult bindingResult = ex.getBindingResult();
+        StringBuilder paramErrorMsg = new StringBuilder();
+        if (bindingResult.hasErrors()) {
+
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            for (ObjectError objectError : allErrors) {
+                String msg = objectError.getDefaultMessage();
+                if (paramErrorMsg.length() == 0) {
+                    paramErrorMsg.append(msg);
+                } else {
+                    paramErrorMsg.append(',');
+                    paramErrorMsg.append(msg);
+                }
+            }
+        }
+        //这里我们输出了错误的日志
+        log.error("参数校验失败! uri:{},错误信息:{}", req.getRequestURI(), paramErrorMsg.toString());
+        //这里我用R_对象去封装返回了
+        return Result.failed(paramErrorMsg.toString());
     }
 }
