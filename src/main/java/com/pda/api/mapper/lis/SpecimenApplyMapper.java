d76a72edb326346a8b9874fea11ac04c4cfd973a
zhanglang <350976906@qq.com>
2023-01-05 21:55:49 +0800

    提交

--- src/main/java/com/pda/api/mapper/lis/SpecimenApplyMapper.java
+++ src/main/java/com/pda/api/mapper/lis/SpecimenApplyMapper.java
@@ -3,6 +3,8 @@ package com.pda.api.mapper.lis;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pda.api.domain.entity.SpecimenCheck;
 import com.pda.api.dto.SpecimenCheckResDto;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -15,4 +17,7 @@ import java.util.List;
 public interface SpecimenApplyMapper extends BaseMapper<SpecimenCheck> {
 
     List<SpecimenCheckResDto> selectSubjectCheck(String patId);
+
+    @Update("update req_master set req_stat = '3' where barcode = #{testNo} and pat_id = #{patId}")
+    int sendSpecimen(@Param("testNo") String testNo,@Param("patId") String patId);
 }
